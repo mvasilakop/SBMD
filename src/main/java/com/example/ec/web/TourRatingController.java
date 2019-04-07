4d039bf9e523ce08b5f8a30b28d2b317b1101644
Michail Vasilakopoulos <bouzouki2015@gmail.com>
2019-04-07 11:28:38 +0100

    Decorated paging APIs with HATEOAS links

--- src/main/java/com/example/ec/web/TourRatingController.java
+++ src/main/java/com/example/ec/web/TourRatingController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -24,10 +26,13 @@ import java.util.stream.Collectors;
 @RequestMapping(path = "/tours/{tourId}/ratings")
 public class TourRatingController {
     private TourRatingService tourRatingService;
-
+    private RatingAssembler assembler;
+    
     @Autowired
-    public TourRatingController(TourRatingService tourRatingService) {
+    public TourRatingController(TourRatingService tourRatingService,
+    							RatingAssembler assembler) {
         this.tourRatingService = tourRatingService;
+        this.assembler = assembler;
     }
 
     protected TourRatingController() {
@@ -69,11 +74,13 @@ public class TourRatingController {
      * @return
      */
     @GetMapping
-    public Page<RatingDto> getAllRatingsForTour(@PathVariable(value = "tourId") int tourId, Pageable pageable) {
+    public PagedResources<RatingDto> getAllRatingsForTour(@PathVariable(value = "tourId") int tourId, 
+    													Pageable pageable,PagedResourcesAssembler<TourRating> pagedAssembler) {
         Page<TourRating> tourRatingPage = tourRatingService.lookupRatings(tourId, pageable);
-        List<RatingDto> ratingDtoList = tourRatingPage.getContent()
-                .stream().map(tourRating -> toDto(tourRating)).collect(Collectors.toList());
-        return new PageImpl<RatingDto>(ratingDtoList, pageable, tourRatingPage.getTotalPages());
+//        List<RatingDto> ratingDtoList = tourRatingPage.getContent()
+//                .stream().map(tourRating -> toDto(tourRating)).collect(Collectors.toList());
+//        return new PageImpl<RatingDto>(ratingDtoList, pageable, tourRatingPage.getTotalPages());
+        return pagedAssembler.toResource(tourRatingPage, assembler);
     }
 
     /**
