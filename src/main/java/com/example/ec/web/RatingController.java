1766203cfd31766445ad8d60cdc9912f53761be8
Michail Vasilakopoulos <bouzouki2015@gmail.com>
2019-04-10 10:23:46 +0100

    Documented APIs with Swagger, programatically

--- src/main/java/com/example/ec/web/RatingController.java
+++ src/main/java/com/example/ec/web/RatingController.java
@@ -1,6 +1,11 @@
 package com.example.ec.web;
 
 import com.example.ec.service.TourRatingService;
+
+import io.swagger.annotations.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -8,12 +13,15 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+
 /**
  * Created by Mary Ellen Bowman
  */
+@Api(description = "API to just pull ratings")
 @RestController
 @RequestMapping(path = "/ratings")
 public class RatingController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RatingController.class);
     private TourRatingService tourRatingService;
 
     private RatingAssembler assembler;
@@ -25,12 +33,19 @@ public class RatingController {
     }
 
     @GetMapping
+    @ApiOperation(value = "Find all ratings")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK") })
     public List<RatingDto> getAll() {
+        LOGGER.info("GET /ratings");
         return assembler.toResources(tourRatingService.lookupAll());
     }
 
     @GetMapping("/{id}")
-    public RatingDto getRating(@PathVariable("id") Integer id) {
+    @ApiOperation(value = "Find ratings by id")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 404, message = "Rating not found") })
+    public RatingDto getRating(//@ApiParam(value = "rating identifier")
+                                   @PathVariable("id") Integer id) {
+        LOGGER.info("GET /ratings/{id}", id);
         return assembler.toResource(tourRatingService.lookupRatingById(id)
                 .orElseThrow(() -> new NoSuchElementException("Rating " + id + " not found"))
         );
@@ -46,6 +61,7 @@ public class RatingController {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoSuchElementException.class)
     public String return400(NoSuchElementException ex) {
+        LOGGER.error("Unable to complete transaction", ex);
         return ex.getMessage();
     }
 }
